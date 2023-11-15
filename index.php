c8352316515aea4767e10fc595baddd9e2e258bd
Mamourou SANGARE <ms284034@iut21.u-bourgogne.fr>
2023-11-15 11:33:41 +0100

    Utilisez les propriétés et méthodes statiques

--- index.php
+++ index.php
@@ -2,34 +2,40 @@
 
 
 class Player{
-    public int $level;    
-    
-
+    private int $level;    
+    public function setLevel(float $level): void
+    {
+        if ($level < 0) {
+        trigger_error('Le level est trop court. (min 1)', E_USER_ERROR);
+    }
+    $this->level = $level;
+    }
+    public function getLevel(): float
+    {
+        return $this->level;
+    }
 
 }
 
 
 class Encounter{
-    public function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
+    public const RESULT_WINNER = 1;
+    public const RESULT_LOSER = -1;
+    public const RESULT_DRAW = 0;
+     public const RESULT_POSSIBILITIES = [RESULT_WINNER, RESULT_LOSER, RESULT_DRAW];
+
+    public static function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
     {
         return 1/(1+(10 ** (($againstLevelPlayerTwo - $levelPlayerOne)/400)));
     }
 
-    public function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
+    public static function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
     {
         if (!in_array($playerOneResult, RESULT_POSSIBILITIES)) {
-        trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', RESULT_POSSIBILITIES)));
+            trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', RESULT_POSSIBILITIES)));
+        }
+
     }
-
-}
-
-
-
-
-
-
-
-
 
 }
 
